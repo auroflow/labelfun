06dcc164d642f2e21b2e89e9d6255f131d9186ac
imbiansl <30548863+imbiansl@users.noreply.github.com>
2021-12-07 00:39:12 +0800

    ignore secret key

--- server/labelfun/settings.py
+++ server/labelfun/settings.py
@@ -1,7 +1,11 @@
 import os
 import sys
 
+from flask.cli import load_dotenv
+
 basedir = os.path.abspath(os.path.dirname(os.path.dirname(__file__)))
+
+load_dotenv()
 
 # SQLite URI compatible
 WIN = sys.platform.startswith('win')
@@ -13,11 +17,13 @@ else:
 
 class BaseConfig:
     SECRET_KEY = os.getenv('SECRET_KEY', 'dev key')
+    QINIU_ACCESS_KEY = os.getenv('QINIU_ACCESS_KEY', 'access key')
+    QINIU_SECRET_KEY = os.getenv('QINIU_SECRET_KEY', 'secret key')
 
 
 class DevelopmentConfig(BaseConfig):
     SQLALCHEMY_DATABASE_URI = sqlite_prefix + \
-        'E:\\Users\\imbiansl\\Desktop\\database.db'
+                              'E:\\Users\\imbiansl\\Desktop\\labelfun\\database.db'
 
 
 class ProductionConfig(BaseConfig):
@@ -25,7 +31,8 @@ class ProductionConfig(BaseConfig):
 
 
 class TestingConfig(BaseConfig):
-    SQLALCHEMY_DATABASE_URI = sqlite_prefix + ':memory:'
+    SQLALCHEMY_DATABASE_URI = sqlite_prefix + 'E:\\Users\\imbiansl\\Desktop' \
+                                              '\\labelfun\\database.db '
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     WTF_CSRF_ENABLED = False
 
