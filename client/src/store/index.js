e6aeffe51fa66e2ba770e051affa71905cbd241f
imbiansl <30548863+imbiansl@users.noreply.github.com>
2021-12-08 20:15:29 +0800

    Fill user state right after vuex is created

--- client/src/store/index.js
+++ client/src/store/index.js
@@ -6,10 +6,18 @@ import message from '@/store/modules/message.js'
 import task from '@/store/modules/task.js'
 Vue.use(Vuex)
 
-export default new Vuex.Store({
+const store = new Vuex.Store({
   modules: {
     user,
     message,
     task,
   },
 })
+
+const userString = localStorage.getItem('user')
+if (userString) {
+  const userData = JSON.parse(userString)
+  store.commit('user/SET_USER_DATA', userData)
+}
+
+export default store
