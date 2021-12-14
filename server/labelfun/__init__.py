dd34c25912d3abb7dfdcc442928f13cae76286a2
imbiansl <30548863+imbiansl@users.noreply.github.com>
2021-12-14 21:03:28 +0800

    Do not add tasks when initializing database

--- server/labelfun/__init__.py
+++ server/labelfun/__init__.py
@@ -1,6 +1,4 @@
 import os
-from datetime import datetime
-from time import sleep
 
 import click
 from apiflask import APIFlask
@@ -9,8 +7,7 @@ from flask.cli import load_dotenv
 from labelfun.apis import api_bp
 from labelfun.extensions import db
 from labelfun.models import UserType
-from labelfun.models.entity import Entity
-from labelfun.models.task import Task
+from labelfun.models.task import Task, Entity
 from labelfun.models.user import User
 from labelfun.settings import config
 
@@ -65,41 +62,42 @@ def register_commands(app: APIFlask):
         admin = User(id=2001, name='Admin', password='abcdefgh',
                      email='admin@email.com', type=1)
 
-        image1 = Entity(type=0, key='key1', thumb_key="t1", path='path1',
-                        status=0)
-        image2 = Entity(type=0, key='key2', thumb_key="t2", path='path2',
-                        status=0)
-        task1 = Task(id=101, status=0, published=True, name='task1',
-                     time=datetime.now(), type=0,
-                     labels=['flower', 'sun', 'sky'])
-        task1.creator = user1
-        task1.entities.append(image1)
-        task1.entities.append(image2)
-        sleep(0.01)
-        video1 = Entity(type=2, key='key3', thumb_key="t3", path='path3',
-                        status=0)
-        video2 = Entity(type=2, key='key4', thumb_key="t4", path='path4',
-                        status=1)
-        task2 = Task(id=102, status=0, name='task2', published=True,
-                     time=datetime.now(), type=2, labels=['dog', 'cat', 'bird'])
-        task2.creator = user2
-        task2.labeler = user1
-        task2.entities.append(video1)
-        task2.entities.append(video2)
-        sleep(0.01)
-        image5 = Entity(type=1, key='key5', thumb_key="t5", path='path5',
-                        status=2)
-        image6 = Entity(type=1, key='key6', thumb_key="t6", path='path6',
-                        status=2)
-        task3 = Task(id=103, status=2, name='task3', published=True,
-                     time=datetime.now(), type=1,
-                     labels=['water', 'air', 'fire'])
-        task3.creator = user1
-        task3.labeler = user2
-        task3.reviewer = admin
-        task3.entities.append(image5)
-        task3.entities.append(image6)
+        # image1 = Entity(type=0, key='key1', thumb_key="t1", path='path1',
+        #                 status=0)
+        # image2 = Entity(type=0, key='key2', thumb_key="t2", path='path2',
+        #                 status=0)
+        # task1 = Task(id=101, status=0, published=True, name='task1',
+        #              time=datetime.now(), type=0,
+        #              labels=['flower', 'sun', 'sky'])
+        # task1.creator = user1
+        # task1.entities.append(image1)
+        # task1.entities.append(image2)
+        # sleep(0.01)
+        # video1 = Entity(type=2, key='key3', thumb_key="t3", path='path3',
+        #                 status=0)
+        # video2 = Entity(type=2, key='key4', thumb_key="t4", path='path4',
+        #                 status=1)
+        # task2 = Task(id=102, status=0, name='task2', published=True,
+        #              time=datetime.now(), type=2, labels=['dog', 'cat', 'bird'])
+        # task2.creator = user2
+        # task2.labeler = user1
+        # task2.entities.append(video1)
+        # task2.entities.append(video2)
+        # sleep(0.01)
+        # image5 = Entity(type=1, key='key5', thumb_key="t5", path='path5',
+        #                 status=2)
+        # image6 = Entity(type=1, key='key6', thumb_key="t6", path='path6',
+        #                 status=2)
+        # task3 = Task(id=103, status=2, name='task3', published=True,
+        #              time=datetime.now(), type=1,
+        #              labels=['water', 'air', 'fire'])
+        # task3.creator = user1
+        # task3.labeler = user2
+        # task3.reviewer = admin
+        # task3.entities.append(image5)
+        # task3.entities.append(image6)
 
-        db.session.add_all([user1, user2, admin, image1, image2, task1,
-                            video1, video2, task2])
+        db.session.add_all([user1, user2, admin])
+        # db.session.add_all([image1, image2, task1,
+        #                     video1, video2, task2])
         db.session.commit()
